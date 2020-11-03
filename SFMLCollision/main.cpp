4cdca47d9924250105ecb35d26b4f692b3f40638
NtdChnn <69079640+NtdChnn@users.noreply.github.com>
2020-11-03 09:48:18 +0700

    finish

--- SFMLCollision/main.cpp
+++ SFMLCollision/main.cpp
@@ -1,14 +1,13 @@
 #include <SFML/Graphics.hpp>
-#include <SFML/Config.hpp>
 #include <iostream>
 #include <windows.h>
-#include <sstream>
+#include <sstream> //score
 using namespace sf;
 using namespace std;
 int main()
 {
 	srand(time(NULL));
-	RenderWindow window(VideoMode(1000, 700), "SFML Tutorial", Style::Close | Style::Resize);
+	RenderWindow window(VideoMode(1000, 700), "SFMLCollosion", Style::Close );
 
 	RectangleShape player(Vector2f(20.0f, 20.0f));
 	player.setFillColor(Color::Magenta);
@@ -17,60 +16,61 @@ int main()
 	opject.setFillColor(Color::Red);
 
 	player.setPosition(Vector2f(100.0f, 350.0f));
-	opject.setPosition(Vector2f(100.0f, 550.0f));
+	opject.setPosition(Vector2f(150.0f, 350.0f));
 
+	//Score
 	int currentScore = 0;
 
 	sf::Font myFont;
-	if (!myFont.loadFromFile("JSChanokNormal.ttf")) {
+	if (!myFont.loadFromFile("JSChanokNormal.ttf")) {}
 
-	}
-
-	// Score 
 	sf::Text score;
 	score.setFont(myFont);
 	score.setFillColor(sf::Color::Green);
 	score.setStyle(sf::Text::Regular);
 	score.setString("Score");
-	score.setCharacterSize(25);
-	score.setPosition(25, 130);
+	score.setCharacterSize(30);
+	score.setPosition(800, 100);
 
 	sf::Text scoreCurrent;
 	scoreCurrent.setFont(myFont);
 	scoreCurrent.setFillColor(sf::Color::Magenta);
 	scoreCurrent.setStyle(sf::Text::Regular);
-	scoreCurrent.setCharacterSize(25);
-	scoreCurrent.setPosition(68, 130);
+	scoreCurrent.setCharacterSize(30);
+	scoreCurrent.setPosition(870, 100);
 
 
 	while (window.isOpen())
 	{
 		if (Keyboard::isKeyPressed(Keyboard::Key::W))
 		{
-			player.move(0.0f, -0.05f);
+			player.move(0.0f, -0.07f);
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Key::A))
 		{
-			player.move(-0.05f, 0.0f);
+			player.move(-0.07f, 0.0f);
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Key::S))
 		{
-			player.move(0.0f, 0.05f);
+			player.move(0.0f, 0.07f);
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Key::D))
 		{
-			player.move(0.05f, 0.0f);
+			player.move(0.07f, 0.0f);
 		}
-		int xOpject = rand() % 600;
-		int yOpject = rand() % 300;
-		if (opject.getGlobalBounds().intersects(player.getGlobalBounds())) {
-			opject.setPosition(Vector2f(xOpject, yOpject));
+
+		int xOp = rand() % 500;
+		int yOp = rand() % 500;
+
+		if (opject.getGlobalBounds().intersects(player.getGlobalBounds()))
+		{
+			opject.setPosition(Vector2f(xOp, yOp));
 			currentScore += 5;
 		}
 
-		stringstream ss;
-		ss << currentScore;
-		scoreCurrent.setString(ss.str().c_str());
+		stringstream scoreShow;
+		scoreShow << currentScore;
+		scoreCurrent.setString(scoreShow.str().c_str());
 
 		window.clear();
 		window.draw(score);
